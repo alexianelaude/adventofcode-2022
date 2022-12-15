97b902beb58ae0dd2cc9a7751a340e731cc970c2
Alexiane Laude <alexiane.laude@datadoghq.com>
2022-12-15 18:07:57 +0100

    day 11 again

--- D11.py
+++ D11.py
@@ -7,8 +7,8 @@ class Monkey:
         self.monkeyFalse = monkeyFalse
         self.itemsInspected = 0
 
-    def testAndThrow(item):
-        self.monkeyTrue if self.test(item) else self.monkeyFalse
+    def testAndThrow(self, item):
+        return self.monkeyTrue if self.test(item) else self.monkeyFalse
 
 
 ops = { "+": int.__add__, "*": int.__mul__ }
@@ -20,19 +20,32 @@ def createMonkeys():
         for i in range(0,56,7):
             items = [int(item.strip(',')) for item in lines[i+1].strip('\n').split(" ")[4:]]
             div = int(lines[i+3].strip('\n').split(" ").pop())
-            test = lambda x : x % div == 0
             (operator, b) = lines[i+2].strip('\n').split(" ")[-2:]
-            operator = ops[operator]
-            op = lambda x : operator(x, x if b == 'old' else int(b))
             monkeyTrue = int(lines[i+4].strip('\n').split(" ").pop())
             monkeyFalse = int(lines[i+5].strip('\n').split(" ").pop())
-            monkeyList.append(Monkey(items,op,test, monkeyTrue, monkeyFalse))
+            monkeyList.append(Monkey(items, lambda x : ops[operator](x, x if b == 'old' else int(b)), lambda x : x % div == 0, monkeyTrue, monkeyFalse))
         f.close()
     return monkeyList
 
-print(createMonkeys())
+def runRound(monkeyList):
+    for monkey in monkeyList:
+        while len(monkey.items) > 0:
+            monkey.itemsInspected += 1
+            item = monkey.items.pop(0)
+            new_worry = monkey.op(item)
+            new_worry = new_worry // 3
+            next_monkey = monkey.testAndThrow(new_worry)
+            monkeyList[next_monkey].items.append(new_worry)
+    return monkeyList
 
+def part1():
+    monkeyList = createMonkeys()
+    for i in range(9):
+        monkeyList = runRound(monkeyList)
+    itemsInspectedList = sorted([monkey.itemsInspected for monkey in monkeyList])
+    return itemsInspectedList[-1] * itemsInspectedList[-2]
 
+print(part1())
 
 
 
